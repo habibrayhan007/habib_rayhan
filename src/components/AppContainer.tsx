727a62447ef548cfae8c34fbf0d4a4748e6620e9
Habib Rayhan <habib@faqi.com.au>
2025-05-26 23:32:56 +0600

    Project Showcase Section Added

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../sections/Hero';
+import AppShowcase from '../sections/ShowcaseSection';
 
 const AppContainer = () => {
   return (
     <div>
       <Hero />
+      <AppShowcase />
     </div>
   )
 }
