aeb68b02cbf168873bfc7f50e124aee58673fa68
Habib Rayhan <habib@faqi.com.au>
2025-05-26 20:18:04 +0600

    WIP-Hero Section Added

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from './sections/Hero';
+import Hero from '../sections/Hero';
 
 const AppContainer = () => {
   return (
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import { words } from '../../constants';
-import Button from '../Button';
-import HeroExperience from '../HeroModels/HeroExperience';
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { words } from '../constants';
+import Button from '../components/Button';
+import HeroExperience from '../components/HeroModels/HeroExperience';
+import AnimatedCounter from '../components/AnimatedCounter';
 
 const Hero = () => {
+
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" }
+    );
+  });
 
   return (
     <section id="hero" className="relative overflow-hidden">
@@ -51,6 +62,8 @@ const Hero = () => {
           </div>
         </figure>
       </div>
+
+      <AnimatedCounter />
     </section>
   )
 }
