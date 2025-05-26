7e1261f991a9157f71c4772dac2147c05febd390
Habib Rayhan <habib@faqi.com.au>
2025-05-26 23:43:32 +0600

    NavBar added

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../sections/Hero';
 import AppShowcase from '../sections/ShowcaseSection';
+import NavBar from './NavBar';
 
 const AppContainer = () => {
   return (
     <div>
+      <NavBar />
       <Hero />
       <AppShowcase />
     </div>
