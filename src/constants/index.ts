8945c8967dde298d1e3f970460096d78727b4270
Habib Rayhan <habib@faqi.com.au>
2025-05-31 03:33:22 +0600

    WIP-Modification Tech Stach & Experience Section

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -91,48 +91,78 @@ const abilities = [
 
 const techStackImgs = [
   {
-    name: "React Developer",
+    name: "React",
     imgPath: "/images/logos/react.png",
   },
   {
-    name: "Python Developer",
-    imgPath: "/images/logos/python.svg",
+    name: "React Native",
+    imgPath: "/images/logos/react.png",
   },
   {
-    name: "Backend Developer",
+    name: "Android",
+    imgPath: "/images/logos/android.png",
+  },
+  {
+    name: "iOS",
+    imgPath: "/images/logos/apple.png",
+  },
+  {
+    name: "JavaScript",
+    imgPath: "/images/logos/javaScript.png",
+  },
+  {
+    name: "TypeScript",
+    imgPath: "/images/logos/typescript.png",
+  },
+  {
+    name: "Firebase",
+    imgPath: "/images/logos/firebase.png",
+  },
+  {
+    name: "Node Js",
     imgPath: "/images/logos/node.png",
   },
   {
-    name: "Interactive Developer",
-    imgPath: "/images/logos/three.png",
+    name: "Python",
+    imgPath: "/images/logos/python.svg",
   },
-  {
-    name: "Project Manager",
-    imgPath: "/images/logos/git.svg",
-  },
+  // {
+  //   name: "Interactive",
+  //   imgPath: "/images/logos/three.png",
+  // },
+  // {
+  //   name: "Project Manager",
+  //   imgPath: "/images/logos/git.svg",
+  // },
 ];
 
 const techStackIcons = [
   {
-    name: "React Developer",
+    name: "React",
     modelPath: "/models/react_logo-transformed.glb",
     scale: 1,
     rotation: [0, 0, 0],
   },
   {
-    name: "Python Developer",
+    name: "Javascript",
+    modelPath: "/models/javascript1.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Python",
     modelPath: "/models/python-transformed.glb",
     scale: 0.8,
     rotation: [0, 0, 0],
   },
   {
-    name: "Backend Developer",
+    name: "Backend",
     modelPath: "/models/node-transformed.glb",
     scale: 5,
     rotation: [0, -Math.PI / 2, 0],
   },
   {
-    name: "Interactive Developer",
+    name: "Interactive",
     modelPath: "/models/three.js-transformed.glb",
     scale: 0.05,
     rotation: [0, 0, 0],
@@ -147,41 +177,40 @@ const techStackIcons = [
 
 const expCards = [
   {
-    review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
-    imgPath: "/images/exp1.png",
-    logoPath: "/images/logo1.png",
-    title: "Frontend Developer",
-    date: "January 2023 - Present",
+    review: "Habib brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+    imgPath: "/images/faq.jpeg",
+    logoPath: "/images/faq.jpeg",
+    title: "Junior Software Engineer",
+    date: "February 2023 - Present",
     responsibilities: [
-      "Developed and maintained user-facing features for the Hostinger website.",
+      "Developed and maintained user-facing features for the Planability Mobile App.",
       "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
-      "Optimized web applications for maximum speed and scalability.",
+      "Optimized App for maximum speed and scalability.",
     ],
   },
   {
-    review: "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
-    imgPath: "/images/exp2.png",
-    logoPath: "/images/logo2.png",
-    title: "Full Stack Developer",
-    date: "June 2020 - December 2023",
+    review: "Habib’s contributions to web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+    imgPath: "/images/mohasagor.png",
+    logoPath: "/images/mohasagor.png",
+    title: "Web Application Developer",
+    date: "November 2022 - January 2023",
     responsibilities: [
-      "Led the development of Docker's web applications, focusing on scalability.",
-      "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
-      "Contributed to open-source projects that were used with the Docker ecosystem.",
+      "develop web applications, focusing on scalability.",
+      "Worked with backend engineers to integrate APIs seamlessly with the frontend.", ,
     ],
   },
-  {
-    review: "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
-    imgPath: "/images/exp3.png",
-    logoPath: "/images/logo3.png",
-    title: "React Native Developer",
-    date: "March 2019 - May 2020",
-    responsibilities: [
-      "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
-      "Improved app performance and user experience through code optimization and testing.",
-      "Coordinated with the product team to implement features based on feedback.",
-    ],
-  },
+  // {
+  //   review: "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+  //   imgPath: "/images/exp3.png",
+  //   logoPath: "/images/logo3.png",
+  //   title: "React Native Developer",
+  //   date: "March 2019 - May 2020",
+  //   responsibilities: [
+  //     "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
+  //     "Improved app performance and user experience through code optimization and testing.",
+  //     "Coordinated with the product team to implement features based on feedback.",
+  //   ],
+  // },
 ];
 
 const expLogos = [
